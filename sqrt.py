d864587a84b09ea336c7e133a86198064d62dfe6
Erika <lordoftherings20179cws17@gmail.com>
2024-01-18 12:26:02 -0800

    added better error message

--- sqrt.py
+++ sqrt.py
@@ -16,7 +16,9 @@ opt = docopt(__doc__)
 def main(number):
   number = int(number)
   print(math.sqrt(number))
-    
+
+if number < 0:
+  raise Exception("n should not a positive number")
 
 if __name__ == "__main__":
   main(opt["--n"])
